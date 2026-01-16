f83b3981e31b5ad6c035096e141702f020629f9d
Luansilv16 <164948628+Luansilv16@users.noreply.github.com>
2026-01-16 10:22:18 -0300

    Update FPSCounter.hx

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -57,7 +57,7 @@ class FPSCounter extends TextField
 		currentFPS = 0;
 		selectable = false;
 		mouseEnabled = false;
-		defaultTextFormat = new TextFormat("_sans", 14, color);
+		defaultTextFormat = new TextFormat("vcr.ttf", 14, color);
 		width = FlxG.width;
 		multiline = true;
 		text = "FPS: ";
